fa69595093046745eaaec54ad7cbb515521161a9
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-05-05 11:00:33 +0300

    photo output

--- js/photoOutput.js
+++ js/photoOutput.js
@@ -1,7 +1,34 @@
 window.onload = function() {
     console.log("fff");
     var foto = [
-        '/img/1.jpg',
-        '/img/2.png',
+        "/img/gallery/11.jpg",
+        "/img/gallery/2.png",
+        "/img/gallery/3.jpg",
+        "/img/gallery/4.jpg",
+        "/img/gallery/5.jpg",
+        "/img/gallery/9.jpg",
+        "/img/gallery/8.jpg",
+        "/img/gallery/1.jpg",
+        "/img/gallery/7.jpg",
+        "/img/gallery/6.jpg",
     ];
+
+    var galleryFoto = document.createElement("div");
+    galleryFoto.classList.add("gallery__items");
+    for (var i = 0; i < 10; i++) 
+        {
+            var d = document.createElement("div");
+            d.classList.add('gallery__item');
+            if (i==0 || i==7){
+                d.classList.add('gallery__item_big');
+            };
+            var img = document.createElement("img");
+            img.src = foto[i];
+
+            d.appendChild(img); //добавляет новые html элементы к существующим
+            galleryFoto.appendChild(d);
+        }
+    var contentGallery = document.getElementById ('content__gallery');
+    contentGallery.appendChild(galleryFoto); 
 };
+
