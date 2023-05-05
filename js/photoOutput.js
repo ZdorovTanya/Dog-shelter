d2981070b0704e1882a2b1b1f60819d709882154
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-05-05 23:28:06 +0300

    gallery popup

--- js/photoOutput.js
+++ js/photoOutput.js
@@ -31,51 +31,71 @@ window.onload = function() {
         "Hood from star wars",
     ];
 
+    // -----------------------------------gallery output------------------------------------------
+
     var galleryFoto = document.createElement("div");
     galleryFoto.classList.add("gallery__items");
     for (var i = 0; i < 10; i++) 
-        {
-            var d = document.createElement("div");
-            d.classList.add('gallery__item');
-            if (i==0 || i==7){
-                d.classList.add('gallery__item_big');
-            };
-            var img = document.createElement("img");
-            img.src = fotoGallery[i];
+    {
+        var dogFoto = document.createElement("div");
+        dogFoto.classList.add('gallery__item');
 
-            d.appendChild(img); //добавляет новые html элементы к существующим
-            galleryFoto.appendChild(d);
-        }
+        if (i==0 || i==7){
+            dogFoto.classList.add('gallery__item_big');
+        };
+
+        var img = document.createElement("img");
+        img.src = fotoGallery[i];
+
+        dogFoto.appendChild(img); 
+        galleryFoto.appendChild(dogFoto);
+
+    }
     var contentGallery = document.getElementById ('content__gallery');
     contentGallery.appendChild(galleryFoto); 
 
+    document.querySelectorAll(".gallery__item img").forEach(img=>{
+        img.onclick = () => {
+            document.querySelector(".pop-up").style.display = 'block';
+            document.querySelector('.pop-up img').src = img.getAttribute('src');
+        }
+    });
+
+
+    document.querySelector('.pop-up span').onclick = () =>{
+        document.querySelector(".pop-up").style.display = 'none';
+    }
+    
+    //------------------------------------------------------------shop output--------------------------------------------------------------
 
     var contentShop = document.getElementById ('products__items');
 
     for (var i = 0; i < 6; i++) 
-        {
-            var card = document.createElement("div");
-            card.classList.add('products__item');
-          
-            var image = document.createElement("div");
-            image.classList.add('products__image');
+    {
+        var card = document.createElement("div");
+        card.classList.add('products__item');
+        
+        var image = document.createElement("div");
+        image.classList.add('products__image');
 
-            var img = document.createElement("img");
-            img.src = shopFoto[i];
+        var img = document.createElement("img");
+        img.src = shopFoto[i];
 
-            var text = document.createElement("div");
-            text.classList.add('products__name');
-            text.innerText = productsName[i];
+        var text = document.createElement("div");
+        text.classList.add('products__name');
+        text.innerText = productsName[i];
 
-            var btnShop = document.createElement("a");
-            btnShop.classList.add('products__button');
-            btnShop.innerText = "Pick up now";
+        var btnShop = document.createElement("a");
+        btnShop.classList.add('products__button');
+        btnShop.innerText = "Pick up now";
 
-            image.appendChild(img);
-            card.appendChild(image); //добавляет новые html элементы к существующим
-            card.appendChild(text);
-            card.appendChild(btnShop);
-            contentShop.appendChild(card); 
-        }
+        image.appendChild(img);
+        card.appendChild(image); //добавляет новые html элементы к существующим
+        card.appendChild(text);
+        card.appendChild(btnShop);
+        contentShop.appendChild(card); 
+    }
+
+
 };
 
